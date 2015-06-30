14606883339a87b1b85244c72715f0f6cc1f7442
kvasilev01 <kvasilev01@gmail.com>
2015-06-30 20:13:00 +0300

    week4
    week4

---
+++ MusicOrganiser.java
@@ -0,0 +1,18 @@
+package com.hackbulgaria.programming51.week4;
+
+public class MusicOrganiser {
+	
+	Vector<Song> songs = new Vector<Song>();
+	private Song[] data;
+	private int size = 0;
+	private int capacity = 10;
+    
+	public MusicOrganiser(){
+		data = (Song[]) new Object[capacity];		
+	}
+	
+	public void addSong(Song s){
+		
+}	
+}
+
