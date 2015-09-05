da908e930f7a0efc844b305312a42e7f150ec6c9
kvasilev01 <kvasilev01@gmail.com>
2015-09-05 16:43:26 +0300

    week4
    week4

--- Song.java
+++ Song.java
@@ -1,18 +1,38 @@
 package com.hackbulgaria.programming51.week4;
 
-public class Song{
-	private String name;
-	private String artist;
-	private int duration;
-	private String album;
+public class Song {
 
-public Song(String name, String artist, int duration, String album){	
-	this.name = name;
-	this.artist = artist;
-	this.duration = duration;
-	this.album = album;
-}
- public String getAlbum(){
-	 return name;
- }
+	private String name = "";
+	private String artist = "";
+	private int duration = 1;
+	private String album = "";
+
+	public Song(String name, String artist, int duration, String album) {
+		this.name = name;
+		this.artist = artist;
+		this.duration = duration;
+		this.album = album;
+	}
+
+	public String toString() {
+		return "Song: " + getName() + ", artist: " + getArtist()
+				+ ", duration: " + getDuration() + ", from album: " + album
+				+ "\n";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
 }
