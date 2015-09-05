6eb3375b34f6cd327658e11721397e6632075e5b
kvasilev01 <kvasilev01@gmail.com>
2015-09-05 16:51:31 +0300

    week3
    week3

---
+++ TravelExpence.java
@@ -0,0 +1,30 @@
+package com.hackbulgaria.programming51.week3;
+
+public class TravelExpence {
+
+	private String type = "";
+	private Employee employee;
+	private double cost = 0.0;
+
+	public TravelExpence(String type, Employee employee, double cost) {
+		this.type = type;
+		this.employee = employee;
+		this.cost = cost;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public double getCost() {
+		return cost;
+	}
+
+	public String toString() {
+		return "Type: " + type + " Employee: " + employee + " netCost: " + cost;
+	}
+}
